585c191c989523521674b57bae42bb5abeb9d09a
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-11-05 14:39:28 +0100

    Added event entity

---
+++ backend/src/main/java/pl/uwm/wateradventure/models/events/EventEntity.java
@@ -0,0 +1,43 @@
+package pl.uwm.wateradventure.models.events;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.uwm.wateradventure.global.WaterAdventureEntity;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+public class EventEntity extends WaterAdventureEntity {
+
+    // TODO -- Create the enum
+//    private String type;
+
+    @Column(name = "is_paid")
+    private Boolean isPaid;
+
+    private Double cost;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    private String city;
+
+    @Column(name = "orderer_email")
+    private String ordererEmail;
+
+    @Column(name = "orderer_first_name")
+    private String ordererFirstName;
+
+    @Column(name = "orderer_last_name")
+    private String ordererLastName;
+
+    @Column(name = "orderer_phone_number")
+    private String ordererPhoneNumber;
+
+}
