116154546159eb9b9cb68fae6194d48d8ac1a20b
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-11-05 12:20:29 +0100

    Initializing backend with Spring Initializer

---
+++ backend/src/test/java/pl/uwm/wateradventure/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uwm.wateradventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
