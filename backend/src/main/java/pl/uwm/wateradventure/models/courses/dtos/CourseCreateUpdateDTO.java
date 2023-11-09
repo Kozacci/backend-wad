65a38dbd70fd5608864122fc39669ec48fbc678f
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-11-09 12:08:07 +0100

    #22 - Adding basic services/controllers/repositories/etc classes - renaming CourseCreateDTO

---
+++ backend/src/main/java/pl/uwm/wateradventure/models/courses/dtos/CourseCreateUpdateDTO.java
@@ -0,0 +1,17 @@
+package pl.uwm.wateradventure.models.courses.dtos;
+
+import lombok.Builder;
+import pl.uwm.wateradventure.models.courses.CourseCity;
+import pl.uwm.wateradventure.models.courses.CourseType;
+
+import java.time.LocalDate;
+
+public record CourseCreateUpdateDTO(CourseType courseType,
+                                    LocalDate dateFrom,
+                                    LocalDate dateTo,
+                                    CourseCity city,
+                                    Integer maxParticipantsNumber) {
+
+    @Builder
+    public CourseCreateUpdateDTO {}
+}
