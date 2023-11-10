f172ec746148ef6cb3583b474f315b6eea229f96
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-11-10 14:55:33 +0100

    #26 - Modifying DB schema, adding Participant Events entity

---
+++ backend/src/main/resources/db/migration/V10__ParticipantEventsTableAddition.sql
@@ -0,0 +1,40 @@
+-- PostgreSQL Script
+-- 08.11.2023 13:00
+-- Deleting unnecessary fields from events entity
+-- Adding Participant Event entity
+
+-- Updating Events table
+ALTER TABLE wateradventure.events
+    DROP COLUMN IF EXISTS orderer_email;
+
+ALTER TABLE wateradventure.events
+    DROP COLUMN IF EXISTS orderer_first_name;
+
+ALTER TABLE wateradventure.events
+    DROP COLUMN IF EXISTS orderer_last_name;
+
+ALTER TABLE wateradventure.events
+    DROP COLUMN IF EXISTS orderer_phone_number;
+
+ALTER TABLE wateradventure.events
+    DROP COLUMN IF EXISTS is_paid;
+
+ALTER TABLE wateradventure.events
+    ADD COLUMN IF NOT EXISTS duration TIME;
+
+ALTER TABLE wateradventure.events
+    ADD COLUMN IF NOT EXISTS max_participants_numer INTEGER;
+
+-- Creating table Participant Events
+CREATE TABLE IF NOT EXISTS wateradventure.participant_events (
+                                                     id BIGINT DEFAULT nextval('wateradventure.id_seq') PRIMARY KEY,
+                                                     orderer_email VARCHAR(40) NOT NULL,
+                                                     orderer_first_name VARCHAR(30) NOT NULL,
+                                                     orderer_last_name VARCHAR(40) NOT NULL,
+                                                     orderer_phone_number VARCHAR(9) NOT NULL,
+                                                     participants_number INTEGER NOT NULL,
+                                                     is_paid BOOLEAN NOT NULL,
+                                                     event_id BIGINT NOT NULL,
+                                                     CONSTRAINT fk_Participant_Events1 FOREIGN KEY (event_id) REFERENCES wateradventure.events (id),
+
+);
