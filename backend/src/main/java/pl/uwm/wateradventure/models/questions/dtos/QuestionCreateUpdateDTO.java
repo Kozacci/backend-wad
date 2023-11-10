c20dd3fa29284977975bfc94161de8061b0e8fc4
krychuu01 <hazzidy125@gmail.com>
2023-11-10 22:32:11 +0100

    #15 - added add question method

--- backend/src/main/java/pl/uwm/wateradventure/models/questions/dtos/QuestionCreateUpdateDTO.java
+++ backend/src/main/java/pl/uwm/wateradventure/models/questions/dtos/QuestionCreateUpdateDTO.java
@@ -1,7 +1,6 @@
 package pl.uwm.wateradventure.models.questions.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import pl.uwm.wateradventure.models.validators.Category;
