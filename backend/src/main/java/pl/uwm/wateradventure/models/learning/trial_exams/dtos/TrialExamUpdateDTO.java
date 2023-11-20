cc251cbebc9d6dc5280bd19673395927c42faec4
krychuu01 <hazzidy125@gmail.com>
2023-11-20 21:53:06 +0100

    #17, #16, #18 - small fixes

--- backend/src/main/java/pl/uwm/wateradventure/models/learning/trial_exams/dtos/TrialExamUpdateDTO.java
+++
@@ -1,7 +0,0 @@
-package pl.uwm.wateradventure.models.learning.trial_exams.dtos;
-
-import jakarta.validation.constraints.NotNull;
-
-public record TrialExamUpdateDTO(@NotNull(message = "")
-                                 Boolean isPassed) {
-}
