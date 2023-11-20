9cc0312011570487ffb08df79595326c56285cbe
krychuu01 <hazzidy125@gmail.com>
2023-11-20 16:35:10 +0100

    #24 - fixed regexp in ParticipantRegisterDTO

--- backend/src/main/java/pl/uwm/wateradventure/models/participants/security/dtos/ParticipantRegisterDTO.java
+++ backend/src/main/java/pl/uwm/wateradventure/models/participants/security/dtos/ParticipantRegisterDTO.java
@@ -23,7 +23,7 @@ public record ParticipantRegisterDTO(@NotBlank(message = "'First name' field mus
                                      String password,
                                      @NotBlank(message = "'Phone number' field must be filled.")
                                      @Size(min = 9, max = 9, message = "'Phone number' must contains 9 digits.")
-                                     @Pattern(regexp = "/d+", message = "'Phone number' field can contain only digits.")
+                                     @Pattern(regexp = "[\\d]+", message = "'Phone number' field can contain only digits.")
                                      String phoneNumber) {
 
     @Builder
