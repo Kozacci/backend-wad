fc33d6ebec11a44901be6c85f2da02c1bfbaff76
krychuu01 <hazzidy125@gmail.com>
2023-12-08 21:51:20 +0100

    #40 - Adding admin modules, routing to admin navbar

---
+++ frontend/src/app/admin/admin-acceses/admin-acceses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-acceses',
+  templateUrl: './admin-acceses.component.html',
+  styleUrls: ['./admin-acceses.component.css']
+})
+export class AdminAccesesComponent {
+
+}
--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -5,12 +5,42 @@ import {RouterModule, Routes} from "@angular/router";
 import {AdminNavbarComponent} from './admin-navbar/admin-navbar.component';
 import {SharedModule} from "../shared/shared.module";
 import { AdminLayoutComponent } from './admin-layout.component';
+import { AdminQuestionsComponent } from './admin-questions/admin-questions.component';
+import { AdminCoursesComponent } from './admin-courses/admin-courses.component';
+import { AdminEventsComponent } from './admin-events/admin-events.component';
+import { AdminPassingComponent } from './admin-passing/admin-passing.component';
+import { AdminAccesesComponent } from './admin-acceses/admin-acceses.component';
+import { AdminCalendarComponent } from './admin-calendar/admin-calendar.component';
 
 const routes: Routes = [
   {
     path: '',
     canActivate: undefined,
     component: AdminHomeComponent
+  },
+  {
+    path: 'questions',
+    component: AdminQuestionsComponent
+  },
+  {
+    path: 'courses',
+    component: AdminCoursesComponent
+  },
+  {
+    path: 'events',
+    component: AdminEventsComponent
+  },
+  {
+    path: 'passing',
+    component: AdminPassingComponent
+  },
+  {
+    path: 'accesses',
+    component: AdminAccesesComponent
+  },
+  {
+    path: 'calendar',
+    component: AdminCalendarComponent
   }
 ]
 
@@ -19,6 +49,12 @@ const routes: Routes = [
     AdminHomeComponent,
     AdminNavbarComponent,
     AdminLayoutComponent,
+    AdminQuestionsComponent,
+    AdminCoursesComponent,
+    AdminEventsComponent,
+    AdminPassingComponent,
+    AdminAccesesComponent,
+    AdminCalendarComponent,
   ],
   exports: [
     AdminLayoutComponent
