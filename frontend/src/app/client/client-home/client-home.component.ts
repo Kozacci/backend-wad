9eb62223c111869cf91ce1992d615d814bb922b6
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-10 13:09:12 +0100

    #39 - Adding client navbar - styling

--- frontend/src/app/client/client-home/client-home.component.ts
+++ frontend/src/app/client/client-home/client-home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-client-home',
   templateUrl: './client-home.component.html',
   styleUrls: ['./client-home.component.css']
 })
-export class ClientHomeComponent {
+export class ClientHomeComponent implements OnInit {
+
+  constructor(private readonly authService: AuthService)
+  {}
+
+  ngOnInit() {
+    this.authService.isLogged();
+    this.authService.messageIfNotLogged();
+  }
 
 }
--- frontend/src/app/client/client-navbar/client-navbar.component.ts
+++ frontend/src/app/client/client-navbar/client-navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+import {AuthService} from "../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-client-navbar',
   templateUrl: './client-navbar.component.html',
   styleUrls: ['./client-navbar.component.css']
 })
-export class ClientNavbarComponent {
+export class ClientNavbarComponent implements OnInit {
 
+  constructor(
+    public authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+  }
 }
