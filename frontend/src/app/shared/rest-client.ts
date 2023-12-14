ed233dc57c2bef0db7f512c833554372c088b11a
krychuu01 <hazzidy125@gmail.com>
2023-12-14 18:41:58 +0100

    #49 - removed unused import

--- frontend/src/app/shared/rest-client.ts
+++ frontend/src/app/shared/rest-client.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {Category, ParticipantEntityDTO, ParticipantLoginDTO, ParticipantRegisterDTO, QuestionFilterDTO} from "./dto";
+import {ParticipantEntityDTO, ParticipantLoginDTO, ParticipantRegisterDTO, QuestionFilterDTO} from "./dto";
 import {Observable} from "rxjs";
 
 @Injectable({
