56ec3c8f4223505918eedd6011d8ace19c711fbf
krychuu01 <hazzidy125@gmail.com>
2023-12-14 18:40:18 +0100

    #41 - temporary added login button to admin-home component

--- frontend/src/app/admin/admin-home/admin-home.component.ts
+++ frontend/src/app/admin/admin-home/admin-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ParticipantLoginDTO} from "../../shared/dto";
+import {AuthService} from "../../shared/services/auth/auth.service";
 
 @Component({
   selector: 'app-admin-home',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AdminHomeComponent {
 
+  constructor(private authService: AuthService) {
+  }
+
+  logowanieDoWywalenia(): void {
+    let parti : ParticipantLoginDTO = <ParticipantLoginDTO> {
+      email: 'admin@email.com',
+      password: 'admin123'
+    }
+    this.authService.login(parti).subscribe(() => console.log("logowanie"));
+  }
 }
