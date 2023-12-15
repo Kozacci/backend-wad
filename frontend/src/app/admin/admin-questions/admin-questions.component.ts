ce9fa1191e2dcbd151f6e9d83c43f401c733559e
krychuu01 <hazzidy125@gmail.com>
2023-12-15 14:56:34 +0100

    #45 - Added paginator to table

--- frontend/src/app/admin/admin-questions/admin-questions.component.ts
+++ frontend/src/app/admin/admin-questions/admin-questions.component.ts
@@ -53,54 +53,86 @@ export class AdminQuestionsComponent {
   ]
 
   questions: QuestionFilterDTO[] = [
-    {
-      id: 1,
-      content: "Pierwsze pytanie...",
-      category: Category.OCHRONA_WOD_PRZED_ZANIECZYSZCZENIEM,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
-    {
-      id: 2,
-      content: "Drugie pytanie...",
-      category: Category.PODSTAWOWE_PRZEPISY_PRAWA,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
-    {
-      id: 3,
-      content: "Trzecie pytanie...",
-      category: Category.PRZEPISY,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
-    {
-      id: 4,
-      content: "Czwarte pytanie...",
-      category: Category.OCHRONA_WOD_PRZED_ZANIECZYSZCZENIEM,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
-    {
-      id: 5,
-      content: "Piąte pytanie...",
-      category: Category.PODSTAWOWE_PRZEPISY_PRAWA,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
-    {
-      id: 6,
-      content: "Szóste pytanie...",
-      category: Category.PRZEPISY,
-      firstAnswer: "Odpowiedź A",
-      secondAnswer: "Odpowiedź B",
-      thirdAnswer: "Odpowiedź C"
-    },
+    // {
+    //   id: 1,
+    //   content: "Pierwsze pytanie...",
+    //   category: Category.OCHRONA_WOD_PRZED_ZANIECZYSZCZENIEM,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 2,
+    //   content: "Drugie pytanie...",
+    //   category: Category.PODSTAWOWE_PRZEPISY_PRAWA,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 3,
+    //   content: "Trzecie pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 4,
+    //   content: "Czwarte pytanie...",
+    //   category: Category.OCHRONA_WOD_PRZED_ZANIECZYSZCZENIEM,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 5,
+    //   content: "Piąte pytanie...",
+    //   category: Category.PODSTAWOWE_PRZEPISY_PRAWA,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 6,
+    //   content: "Szóste pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 6,
+    //   content: "Szóste pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 6,
+    //   content: "Szóste pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 6,
+    //   content: "Szóste pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
+    // {
+    //   id: 6,
+    //   content: "Szóste pytanie...",
+    //   category: Category.PRZEPISY,
+    //   firstAnswer: "Odpowiedź A",
+    //   secondAnswer: "Odpowiedź B",
+    //   thirdAnswer: "Odpowiedź C"
+    // },
   ];
 
 }
