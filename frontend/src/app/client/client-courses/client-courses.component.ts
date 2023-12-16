c288763be3a2d33da4440ead860460c1f3295b12
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-16 18:55:35 +0100

    #54 - Adding client courses view - modified styles

--- frontend/src/app/client/client-courses/client-courses.component.ts
+++ frontend/src/app/client/client-courses/client-courses.component.ts
@@ -15,13 +15,68 @@ export class ClientCoursesComponent {
       includes: [
         'Szkolenie on-line (e-learning)',
         'Szkolenie stacjonarne',
-        // inne punkty
+        'Egzamin kończący',
+        'Certyfikat ukończenia szkolenia'
       ],
-      price: '550zł / OSOBA',
-      image: 'path_to_image.jpg',
-      // inne właściwości kursu
+      price: '750zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-1.png',
     },
-    // inne kursy...
+    {
+      title: 'JACHTOWY STERNIK MORSKI',
+      location: 'Sopot | Olecko',
+      includes: [
+        'Szkolenie on-line (e-learning)',
+        'Szkolenie stacjonarne',
+        'Egzamin kończący',
+        'Certyfikat ukończenia szkolenia'
+      ],
+      price: '850zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-2.png',
+    },
+    {
+      title: 'MORSKI STERNIK MOTOROWODNY',
+      location: 'Sopot | Olecko',
+      includes: [
+        'Szkolenie on-line (e-learning)',
+        'Szkolenie stacjonarne',
+        'Egzamin kończący',
+        'Certyfikat ukończenia szkolenia'
+      ],
+      price: '950zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-3.png',
+    },
+    {
+      title: 'ŻEGLARZ JACHTOWY',
+      location: 'Sopot | Olecko',
+      includes: [
+        'Szkolenie on-line (e-learning)',
+        'Szkolenie stacjonarne',
+        'Egzamin kończący',
+        'Certyfikat ukończenia szkolenia'
+      ],
+      price: '750zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-4.png',
+    },
+    {
+      title: 'WARSZTATY NAWIGACYJNE',
+      location: 'Sopot | Olecko',
+      includes: [
+        'Szkolenie stacjonarne',
+        'Certyfikat ukończenia warsztatu'
+      ],
+      price: '350zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-5.png',
+    },
+    {
+      title: 'REJSY STAŻOWE',
+      location: 'Sopot | Olecko',
+      includes: [
+        'Szkolenie stacjonarne',
+        'Certyfikat ukończenia stażu'
+      ],
+      price: '450zł / OSOBA',
+      image: 'assets/images/client/overlay/courses/course-6.png',
+    },
   ];
 
   constructor(
@@ -31,7 +86,7 @@ export class ClientCoursesComponent {
   }
 
 
-  goToDetails(course: any) {
+  goToCourseDetails(course: any) {
     // Logika nawigacji do szczegółów kursu, np.:
     this.pathService.navigate('/courses-top');
   }
