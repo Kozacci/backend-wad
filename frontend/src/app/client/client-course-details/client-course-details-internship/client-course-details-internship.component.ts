1672e2ee617bb5573c4cdd328bab693be0508deb
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-17 21:35:12 +0100

    #54 - Adding client courses view - Adding client course details internship component

---
+++ frontend/src/app/client/client-course-details/client-course-details-internship/client-course-details-internship.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-course-details-internship',
+  templateUrl: './client-course-details-internship.component.html',
+  styleUrls: ['./client-course-details-internship.component.css']
+})
+export class ClientCourseDetailsInternshipComponent {
+
+}
