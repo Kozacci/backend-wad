45478137621631c877bf5572706c60833155c0a7
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-19 00:01:26 +0100

    #54 - Adding client courses view - added sign on a course method + additionally modified styles

--- frontend/src/app/client/client-course-details/client-course-details.component.ts
+++ frontend/src/app/client/client-course-details/client-course-details.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit, Output} from '@angular/core';
 import {CourseFilterDTO} from "../../shared/dto";
 import {RestClient} from "../../shared/rest-client";
+import {AuthService} from "../../shared/services/auth/auth.service";
+import {MessageService} from "primeng/api";
+import {HttpResponseHandlerService} from "../../shared/services/http-response-handler.service";
 
 export interface CourseDetails {
   type: string,
@@ -25,20 +28,76 @@ export interface CourseDetails {
 })
 export class ClientCourseDetailsComponent implements OnInit {
 
+  modalVisiblity: boolean = false;
+  selectedAvailableCourseId: number | null = null;
   @Input()
   course: CourseDetails = <CourseDetails>{};
   @Output()
   availableCourses: CourseFilterDTO[] = <CourseFilterDTO[]>{};
 
   constructor(
-    private readonly restClient: RestClient
+    private readonly restClient: RestClient,
+    public readonly authService: AuthService,
+    private readonly messageService: MessageService,
+    private readonly httpResponseHandlerService: HttpResponseHandlerService
   ) {}
 
   ngOnInit(): void {
     this.checkAvailableCourses()
   }
 
-  // TODO - 403 error without loggin' in
+  signInOnCourseModal() {
+    if(this.authService.isLogged()) {
+      this.modalVisiblity = true;
+    }
+    else {
+      this.messageService.add({
+        life:5000,
+        severity:'error',
+        summary:'Logowanie',
+        detail:"Musisz się najpierw zalogować!"}
+      )
+    }
+  }
+
+  signInOnCourseModalClose() {
+    this.modalVisiblity = false;
+  }
+
+  selectAvailableCourse(id: number) {
+    this.selectedAvailableCourseId = id;
+  }
+
+  // Todo - form and placeholder taken from getParticipantByEmail(cachEmail) rest method
+  // Todo - everything same for events
+  // Todo - e-learning from beginning
+  singInOnCourse() {
+    const participantId = parseInt(sessionStorage.getItem('cacheId')?? '', 10) ;
+    if (this.selectedAvailableCourseId != null) {
+      this.restClient.signInOnCourse(participantId, this.selectedAvailableCourseId)
+        .subscribe( response => {
+            this.messageService.add({
+              life: 4000,
+              severity: 'success',
+              summary: 'Zapis na kurs',
+              detail: "Udało Ci się pomyślnie zapisać na kurs"
+            })
+            this.modalVisiblity = false;
+          },
+          error => {
+            this.messageService.add({
+              life: 4000,
+              severity: 'error',
+              summary: 'Zapis na kurs',
+              detail: error.error.message
+            });
+          }
+        )
+        ;
+    }
+  }
+
+  // Todo - 403 error without loggin' in
   checkAvailableCourses() {
     this.restClient.getCoursesByFilters(
       this.course.type,
