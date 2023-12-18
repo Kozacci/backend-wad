d1f3fbf378bfb885fe1120c567c9c851dc446aa0
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-18 16:26:31 +0100

    #54 - Adding client courses view - Modified client courses details sailor component

--- frontend/src/app/client/client-course-details/client-course-details-sailor/client-course-details-sailor.component.ts
+++ frontend/src/app/client/client-course-details/client-course-details-sailor/client-course-details-sailor.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CourseDetails} from "../client-course-details.component";
+import {ClientCourseDetailsService} from "../client-course-details.service";
 
 @Component({
   selector: 'app-client-course-details-sailor',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class ClientCourseDetailsSailorComponent {
 
+  courseSailor: CourseDetails = <CourseDetails>{};
+
+  constructor(
+    private readonly clientCourseDetailsService: ClientCourseDetailsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.clientCourseDetailsService.ustawDaneDomyslne(
+      this.courseSailor,
+      'ZEGLARZ_JACHTOWY',
+      'Żeglarz jachtowy',
+      'assets/images/client/overlay/courses-details/szczegoly-zeglarz-jachtowy.jpg',
+      [
+        'Uprawnienie do prowadzenie jachtów motorowych po wodach śródlądowych',
+        'Uprawnienie do prowadzenia jachtów o długości kadłuba do 12 m po morskich wodach wewnętrznych oraz pozostałych wodach morskich w strefie do 2 Mm od brzegu, w porze dziennej.',
+      ],
+      [
+        'Ukończone 14 lat',
+        'Oświadczenie rodziców lub opiekuna prawnego w przypadku niepełnoletniego',
+        'Uiszczenie opłaty za egzamin wg. rozporządzenia Ministra Sportu i Turystyki'
+      ],
+      "3 dni",
+      'Jachtownik 3000 (2018)',
+      'Dostęp do bazy pytań (e-learning) i egzaminów próbnych',
+      'Egzamin w cenie kursu',
+      'Certyfikat po ukończeniu szkolenia',
+      'Ubezpieczenie NNW i środki ratunkowe na czas zajęć praktycznych',
+      '750'
+    );
+  }
 }
