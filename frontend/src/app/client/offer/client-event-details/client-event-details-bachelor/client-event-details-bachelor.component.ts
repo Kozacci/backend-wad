6900d7093de52ad32f93a12a6ef1edb1b565dd55
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-24 09:55:14 +0100

    #61 - Adding client events view - event details-bachelor init

---
+++ frontend/src/app/client/offer/client-event-details/client-event-details-bachelor/client-event-details-bachelor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-event-details-bachelor',
+  templateUrl: './client-event-details-bachelor.component.html',
+  styleUrls: ['./client-event-details-bachelor.component.css']
+})
+export class ClientEventDetailsBachelorComponent {
+
+}
