bcdc9599f9e2abcd53074825f9e7fec91a21d004
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-24 09:55:42 +0100

    #61 - Adding client events view - event details-scooter init

---
+++ frontend/src/app/client/offer/client-event-details/client-event-details-scooter/client-event-details-scooter.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientEventDetailsScooterComponent } from './client-event-details-scooter.component';
+
+describe('ClientEventDetailsScooterComponent', () => {
+  let component: ClientEventDetailsScooterComponent;
+  let fixture: ComponentFixture<ClientEventDetailsScooterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ClientEventDetailsScooterComponent]
+    });
+    fixture = TestBed.createComponent(ClientEventDetailsScooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/client/offer/client-event-details/client-event-details-scooter/client-event-details-scooter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-event-details-scooter',
+  templateUrl: './client-event-details-scooter.component.html',
+  styleUrls: ['./client-event-details-scooter.component.css']
+})
+export class ClientEventDetailsScooterComponent {
+
+}
