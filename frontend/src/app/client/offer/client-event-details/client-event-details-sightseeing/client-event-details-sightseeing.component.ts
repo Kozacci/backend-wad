28441002ed39dfb12b4872581ec59fabe86dfec5
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-24 09:55:51 +0100

    #61 - Adding client events view - event details-sightseeing init

---
+++ frontend/src/app/client/offer/client-event-details/client-event-details-sightseeing/client-event-details-sightseeing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-event-details-sightseeing',
+  templateUrl: './client-event-details-sightseeing.component.html',
+  styleUrls: ['./client-event-details-sightseeing.component.css']
+})
+export class ClientEventDetailsSightseeingComponent {
+
+}
