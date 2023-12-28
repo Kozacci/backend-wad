8e1334c8931340725b5cd4ee06da481361e47110
krychuu01 <hazzidy125@gmail.com>
2023-12-28 16:37:27 +0100

    #69 - added basic impl of admin-events

--- frontend/src/app/admin/admin-events/admin-events.component.ts
+++ frontend/src/app/admin/admin-events/admin-events.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {EventFilterDTO, NameValueNull} from "../../shared/dto";
+import {RestClient} from "../../shared/rest-client";
+import {HttpResponseHandlerService} from "../../shared/services/http-response-handler.service";
 
 @Component({
   selector: 'app-admin-events',
@@ -6,5 +9,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-events.component.css']
 })
 export class AdminEventsComponent {
+
+  eventType: NameValueNull = null;
+  eventCity: NameValueNull = null;
+  sortBy: NameValueNull = null;
+  maxParticipantsNumber: number | undefined = undefined;
+  cost: number | undefined = undefined;
+  adminSearch: boolean = true;
+  eventsList: EventFilterDTO[] = [];
+
+  constructor(private restClient: RestClient,
+              protected responseHandlerService: HttpResponseHandlerService) {
+  }
+
+  findEvents(): void {
+    this.restClient.getEventsByFilters(this.eventType?.name,
+                    this.eventCity?.name,
+                    this.cost,
+                    this.maxParticipantsNumber,
+                    this.sortBy?.value,
+                    this.adminSearch
+      ).subscribe(response => {
+      this.eventsList = response;
+    })
+  }
+
+  showAddEventModal(): void {
+
+  }
+
+  insertDataIntoEditEventModal(eventFilterDTO: EventFilterDTO): void {
+
+  }
+
+  // select input values
+
+  eventTypes= [
+    { name: "REJS_WIDOKOWY", value: "Rejs widokowy"},
+    { name: "PANIENSKI", value: "Wieczór panieński"},
+    { name: "KAWALERSKI", value: "Wieczór kawalerski"},
+    { name: "WYNAJEM_SKUTERA", value: "Wynajem skutera wodnego"},
+    { name: "EVENT_DLA_FIRMY", value: "Event dla firmy"}
+  ];
+
+  eventCities = [
+    { name: "SOPOT", value: "Sopot"},
+    { name: "OLECKO", value: "Olecko"},
+    { name: "GDANSK", value: "Gdańsk"}
+  ]
+
+  sortByValues = [
+    { name: "Rodzaj", value: "type"},
+    { name: "Miasto", value: "city"},
+    { name: "Koszt", value: "cost"},
+    { name: "Maksymalna liczba kursantów", value: "maxParticipantsNumber"}
+  ]
 
 }
