e2e5a09a973e8ff8966f6fa923e4b380b6bd488f
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-28 12:04:48 +0100

    #67 - Adding My Events component to Client Layout - added some logic

--- frontend/src/app/client/client-my-events/client-my-events.component.ts
+++ frontend/src/app/client/client-my-events/client-my-events.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {EventFilterDTO} from "../../shared/dto";
+import {EventFilterDTO, EventType, ParticipantEventFilterDTO} from "../../shared/dto";
 import {RestClient} from "../../shared/rest-client";
 import {PathService} from "../../shared/services/path.service";
 
@@ -10,7 +10,7 @@ import {PathService} from "../../shared/services/path.service";
 })
 export class ClientMyEventsComponent implements OnInit {
 
-  events: EventFilterDTO[] = [];
+  events: ParticipantEventFilterDTO[] = [];
 
   constructor(private readonly restClient: RestClient,
               private readonly pathService: PathService) {
@@ -33,20 +33,35 @@ export class ClientMyEventsComponent implements OnInit {
   // TODO 1 -- my courses + my events + navigate after signing on events/courses
   // TODO 2 -- e-learning (stats, results, learning with 3 types of it)
 
-  getImage(event: EventFilterDTO): string {
-    if(event.type == 'PANIENSKI') {
-      return 'assets/images/client/overlay/events/event-1.png';
+  getImage(event: ParticipantEventFilterDTO): string {
+    switch (event.type) {
+      case EventType.PANIENSKI:
+        return 'assets/images/client/overlay/events/event-1.png';
+      case EventType.KAWALERSKI:
+        return 'assets/images/client/overlay/events/event-3.png';
+      case EventType.REJS_WIDOKOWY:
+        return 'assets/images/client/overlay/events/event-4.png';
+      case EventType.EVENT_DLA_FIRMY:
+        return 'assets/images/client/overlay/events/event-5.png';
+      default:
+        return 'assets/images/client/overlay/events/event-6.png';
     }
-    if(event.type == 'KAWALERSKI') {
-      return 'assets/images/client/overlay/events/event-3.png';
-    }
-    if(event.type == 'REJS_WIDOKOWY') {
-      return 'assets/images/client/overlay/events/event-4.png';
-    }
-    if(event.type == 'EVENT_DLA_FIRMY') {
-      return 'assets/images/client/overlay/events/event-5.png';
-    }
-    return 'assets/images/client/overlay/events/event-6.png';
+  }
+
+  getDate(event: ParticipantEventFilterDTO): string {
+    return event.date.toString().split('T')[0];
+  }
+
+  getHours(event: ParticipantEventFilterDTO): string {
+    return event.date.toString().split('T')[1];
+  }
+
+  goToEventDetails(event: ParticipantEventFilterDTO) {
+
+  }
+
+  cancelParticipantEvent(event: ParticipantEventFilterDTO) {
+
   }
 
 }
