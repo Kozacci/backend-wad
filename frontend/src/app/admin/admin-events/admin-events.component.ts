4255443b985901174d490b20cdf9f02f1b707f60
krychuu01 <hazzidy125@gmail.com>
2023-12-29 01:01:29 +0100

    #69 - added add event modal

--- frontend/src/app/admin/admin-events/admin-events.component.ts
+++ frontend/src/app/admin/admin-events/admin-events.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
-import {EventFilterDTO, NameValueNull} from "../../shared/dto";
+import {Component} from '@angular/core';
+import {
+  EventCity,
+  EventCreateUpdateDTO,
+  EventEntityDTO,
+  EventFilterDTO, EventType,
+  GroupedErrorDTO,
+  NameValueNull
+} from "../../shared/dto";
 import {RestClient} from "../../shared/rest-client";
 import {HttpResponseHandlerService} from "../../shared/services/http-response-handler.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormService} from "../../shared/services/form/form.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-admin-events',
@@ -17,9 +27,31 @@ export class AdminEventsComponent {
   cost: number | undefined = undefined;
   adminSearch: boolean = true;
   eventsList: EventFilterDTO[] = [];
+  groupedErrors: GroupedErrorDTO[] = [];
+  addEventModalVisible: boolean = false;
+  eventTypeToAdd: NameValueNull = null;
+  eventCityToAdd: NameValueNull = null;
+  eventToAdd: EventCreateUpdateDTO = <EventCreateUpdateDTO><unknown>{
+    cost: null, city: null, maxParticipantsNumber: null, date: null, duration: null, type: null
+  };
+  private today = new Date();
+  addEventFormGroup= new FormGroup({
+    date: new FormControl(new Date(this.today.getFullYear(), this.today.getMonth(),
+        this.today.getDate(), 12, 0, 0, 0),
+      [Validators.required]),
+    duration: new FormControl(new Date(2023, 0, 1,
+        1, 0, 0 ,0),
+      [Validators.required]),
+    maxParticipantsNumber: new FormControl(this.eventToAdd.maxParticipantsNumber,
+      [Validators.required]),
+    cost: new FormControl(this.eventToAdd.cost,
+      [Validators.required])
+  })
 
   constructor(private restClient: RestClient,
-              protected responseHandlerService: HttpResponseHandlerService) {
+              protected responseHandlerService: HttpResponseHandlerService,
+              protected formService: FormService,
+              private datePipe: DatePipe) {
   }
 
   findEvents(): void {
@@ -29,16 +61,68 @@ export class AdminEventsComponent {
                     this.maxParticipantsNumber,
                     this.sortBy?.value,
                     this.adminSearch
-      ).subscribe(response => {
-      this.eventsList = response;
+      ).subscribe(foundedEvents => {
+      this.eventsList = foundedEvents;
     })
   }
 
-  showAddEventModal(): void {
+  addEvent(): void {
+    this.groupedErrors = [];
+    this.eventToAdd.type = <EventType>this.eventTypeToAdd?.value;
+    this.eventToAdd.city = <EventCity>this.eventCityToAdd?.value;
+    this.eventToAdd.date = <Date>this.addEventFormGroup.value.date;
+    this.eventToAdd.duration = this.formatDateToLocalTimeString(<Date>this.addEventFormGroup.value.duration);
+    this.eventToAdd.cost = <number>this.addEventFormGroup.value.cost;
+    this.eventToAdd.maxParticipantsNumber = <number>this.addEventFormGroup.value.maxParticipantsNumber;
+    this.restClient.addEvent(this.eventToAdd).subscribe(response => {
+      this.responseHandlerService.showSuccessPToast("Dodanie eventu", "Event został dodany pomyślnie.");
+      this.appendEventToTable(response);
+    }, error => {
+      this.groupedErrors = this.responseHandlerService.getErrorsBelowInputs(error);
+      this.responseHandlerService.handleErrorsPtoasts(error);
+    })
+  }
 
+  appendEventToTable(addedEvent: EventEntityDTO): void {
+    this.eventsList = this.eventsList.concat([<EventFilterDTO>{
+      id: addedEvent.id,
+      city: <EventCity>addedEvent.city, // możliwe ze bedzie trzeba dodać mapowanie, sprawdzam czy działa
+      type: <EventType>addedEvent.type,
+      cost: addedEvent.cost,
+      date: addedEvent.date,
+      duration: addedEvent.duration,
+      maxParticipantsNumber: addedEvent.maxParticipantsNumber,
+      assignedParticipants: addedEvent.assignedParticipants,
+    }])
   }
 
   insertDataIntoEditEventModal(eventFilterDTO: EventFilterDTO): void {
+
+  }
+
+  formatDateToLocalTimeString(date: Date) {
+    if (date == null) return null;
+    date.setSeconds(0);
+    return this.datePipe.transform(date, 'HH:mm:ss')
+  }
+
+  showAddEventModal(): void {
+    this.addEventModalVisible = true;
+  }
+
+  closeAddEventModal(): void {
+    this.addEventModalVisible = false;
+    this.groupedErrors = [];
+    this.addEventFormGroup.clearAsyncValidators();
+  }
+
+  // getters & setters
+
+  setDateAtEventAddModal(date: Date) {
+
+  }
+
+  setDurationAtEventAddModal(date: Date) {
 
   }
 
