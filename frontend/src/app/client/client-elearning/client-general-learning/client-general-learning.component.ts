a344622915c5d22b75739540afb62f7fb186ad32
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-29 19:23:15 +0100

    #68 - Adding E-Learning component to Client Layout - added elearning service

--- frontend/src/app/client/client-elearning/client-general-learning/client-general-learning.component.ts
+++ frontend/src/app/client/client-elearning/client-general-learning/client-general-learning.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-client-general-learning',
   templateUrl: './client-general-learning.component.html',
   styleUrls: ['./client-general-learning.component.css']
 })
-export class ClientGeneralLearningComponent {
+export class ClientGeneralLearningComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
 
 }
