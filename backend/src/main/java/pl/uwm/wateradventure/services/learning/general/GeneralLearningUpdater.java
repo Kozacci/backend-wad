3cc923771af93f3cba3711a029c76f3bffe17f73
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-12-30 13:31:39 +0100

    #68 - Adding E-Learning component to Client Layout - modified general learning update logic and dtos

--- backend/src/main/java/pl/uwm/wateradventure/services/learning/general/GeneralLearningUpdater.java
+++ backend/src/main/java/pl/uwm/wateradventure/services/learning/general/GeneralLearningUpdater.java
@@ -3,7 +3,6 @@ package pl.uwm.wateradventure.services.learning.general;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.uwm.wateradventure.models.learning.general.GeneralLearningEntity;
-import pl.uwm.wateradventure.models.learning.general.dtos.GeneralLearningUpdateDTO;
 
 @Component
 @RequiredArgsConstructor
@@ -11,11 +10,18 @@ class GeneralLearningUpdater {
 
     private final GeneralLearningRepository repository;
 
-    public GeneralLearningEntity update(GeneralLearningEntity generalLearningToChange, GeneralLearningUpdateDTO dto) {
-        generalLearningToChange.addQuestionsAnswered(dto.questionsAnswered());
-        generalLearningToChange.addCorrectAnswers(dto.correctAnswers());
-        repository.saveAndFlush(generalLearningToChange);
-        return generalLearningToChange;
+    public GeneralLearningEntity update(
+            GeneralLearningEntity generalLearningToChange,
+            Boolean isCorrectAnswer
+    ) {
+        if(isCorrectAnswer) {
+            generalLearningToChange.addCorrectAnswers(1);
+            generalLearningToChange.addQuestionsAnswered(1);
+        }
+        else {
+            generalLearningToChange.addQuestionsAnswered(1);
+        }
+        return repository.saveAndFlush(generalLearningToChange);
     }
 
 }
