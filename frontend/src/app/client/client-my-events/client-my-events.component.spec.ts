b04ad668b3b5653021bd67c03dc994e0df8a8eda
IgorKawczynski <kluseczkibabuni94@gmail.com>
2024-01-04 22:54:07 +0100

    #74 - Adding Jasmine Unit tests for Client Layout - my events tests

--- frontend/src/app/client/client-my-events/client-my-events.component.spec.ts
+++ frontend/src/app/client/client-my-events/client-my-events.component.spec.ts
@@ -1,21 +1,75 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {fakeAsync, tick} from '@angular/core/testing';
 import {ClientMyEventsComponent} from './client-my-events.component';
+import {RestClient} from "../../shared/rest-client";
+import {MessageService} from "primeng/api";
+import {of, throwError} from "rxjs";
+import {
+  EventCity, EventType,
+  ParticipantEventFilterDTO
+} from "../../shared/dto";
+import {PathService} from "../../shared/services/path.service";
 
+/**
+ * Jasmine Unit Test to check client my events component
+ */
 describe('ClientMyEventsComponent', () => {
   let component: ClientMyEventsComponent;
-  let fixture: ComponentFixture<ClientMyEventsComponent>;
+  let pathServiceSpy: jasmine.SpyObj<PathService>;
+  let restClientSpy: jasmine.SpyObj<RestClient>;
+  let messageServiceSpy: jasmine.SpyObj<MessageService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ClientMyEventsComponent]
-    });
-    fixture = TestBed.createComponent(ClientMyEventsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    restClientSpy = jasmine.createSpyObj('RestClient', ['getParticipantEventsByFilters', 'deleteAssigningForEvent']);
+    messageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
+
+    component = new ClientMyEventsComponent(restClientSpy, pathServiceSpy, messageServiceSpy);
+
+    // Setting default mocks
+    restClientSpy.deleteAssigningForEvent.and.returnValue(of({}));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  /**
+   * It checks if getParticipantEvents method properly launches
+   */
+  it('should update events when getParticipantEvents is called', () => {
+
+    const mockEvents: ParticipantEventFilterDTO[] = [];
+
+    restClientSpy.getParticipantEventsByFilters.and.returnValue(of(mockEvents));
+
+    component.ngOnInit();
+
+    expect(component.events).toEqual(mockEvents);
+    expect(restClientSpy.getParticipantEventsByFilters).toHaveBeenCalled();
   });
+
+  /**
+   *  It checks if failure cancellation of assigning on event works properly
+   *  by asserting console error
+   *  TODO - create unit test for successful call of this method
+   */
+  it('should log an error to the console on cancelParticipantEvent failure', fakeAsync(() => {
+    const errorResponse = { error: { message: 'Błąd rezygnacji z eventu' } };
+    restClientSpy.deleteAssigningForEvent.and.returnValue(throwError(() => errorResponse));
+    spyOn(console, 'error');
+
+    const event = {
+      eventId: -1,
+      type: EventType.EVENT_DLA_FIRMY,
+      city: EventCity.SOPOT,
+      cost: 500,
+      assignedParticipants: 16,
+      maxParticipantsNumber: 15,
+      isPaid: true,
+      date: new Date(Date.now()),
+      duration: new Date(Date.now()),
+      participantEventId: -5,
+      ordererLastName: '1',
+      ordererEmail: 'testEmaill'
+    };
+
+    component.cancelParticipantEvent(event);
+    tick();
+    expect(console.error).toHaveBeenCalledWith('Błąd rezygnacji z eventu', errorResponse);
+  }));
 });
